33d4da1446876c4494a2dccb78f89139983a84b1
beck <1504068285@qq.com>
2026-01-16 22:30:09 +0800

    fix: Auto-remove build containers after completion

--- src/commands/build/docker.rs
+++ src/commands/build/docker.rs
@@ -84,6 +84,7 @@ pub fn setup_docker_run_args(
 
     let mut docker_run_args = vec![
         "run".to_string(),
+        "--rm".to_string(),
         "-u".to_string(),
         "foc-user".to_string(),
         "--name".to_string(),
