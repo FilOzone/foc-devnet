b2689b4512e8d1f5cf97e1f12d6173368a0574c1
RedPanda <redpanda@filoz.org>
2026-02-03 20:49:03 +0530

    fix: remove unused MAX_PROVIDER_ID constant

--- src/commands/start/endorsement/constants.rs
+++ src/commands/start/endorsement/constants.rs
@@ -5,6 +5,3 @@ pub const ENDORSEMENT_CONTAINER_PREFIX: &str = "foc-pdp-endorse";
 
 /// Wait time after endorsement transaction (seconds)
 pub const ENDORSEMENT_TX_WAIT_SECS: u64 = 10;
-
-/// Maximum provider ID value (ProviderIdSet limitation)
-pub const MAX_PROVIDER_ID: u64 = 0xFFFFFFFF;
