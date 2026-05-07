b3773f07426447092dc9f60b3772b2a1efd1d6bf
Piotr Galar <piotr.galar@gmail.com>
2026-05-07 13:20:24 +0100

    feat: support latest synapse (#107)
    Co-authored-by: Rod Vagg <rod@vagg.org>

--- scenarios/run.py
+++ scenarios/run.py
@@ -26,7 +26,8 @@ from scenarios.report import TestResult, write_report
 ORDER = [
     ("test_containers", 5),
     ("test_basic_balances", 10),
-    ("test_storage_e2e", 100),
+    ("test_storage_e2e", 200),
+    ("test_multi_copy_upload", 600),
     ("test_caching_subsystem", 200),
 ]
 
